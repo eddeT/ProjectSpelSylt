ec946b90b6784c5ed893d1272946964e878042d1
Teodor Norén <teodornoren@gmail.com>
2016-02-22 10:40:33 +0100

    Particle Self Destruction

---
+++ SpelSylt/Assets/Scripts/AutoDestroyParticleSystem.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoDestroy : MonoBehaviour
+{
+    ParticleSystem particleSystem;
+    // Use this for initialization
+    void Awake()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        if(particleSystem.isPlaying == true)
+        {
+            return;
+        }
+        Destroy(gameObject);
+    }
+}
